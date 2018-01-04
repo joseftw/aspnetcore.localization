1e08886ad4443d436e88d04ecc2e653bdcfb9d41
joseftw <ottossonjosef@gmail.com>
2018-01-04 15:20:36 +0100

    removes unused files and adds localization

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStringLocalizer<HomeController> _stringLocalizer;
+
+        public HomeController(IStringLocalizer<HomeController> stringLocalizer)
+        {
+            _stringLocalizer = stringLocalizer ?? throw new ArgumentNullException(nameof(stringLocalizer));
+        }
+
+        public IActionResult Index()
+        {
+            var localizedHeading = this._stringLocalizer["Heading"].Value;
+            return View("~/Views/Home/Index.cshtml", localizedHeading);
+        }
+    }
+}
